2d8930b59250ecc474cc1a44af6d66d411e120cd
Gabriel Guizardi Cardoso <gabriel.guizardi@leavening.com>
2023-04-19 18:47:32 -0300

    Adicionando comentários no módulo query search

--- noob-code-mini-blog/app/models/concerns/query/post_query.rb
+++ noob-code-mini-blog/app/models/concerns/query/post_query.rb
@@ -7,6 +7,7 @@ module PostQuery
   end
 
   module ClassMethods
+    # Formata os parâmetros recebidos para um formato adequado para ser utilizado pelo Ransack.
     def format_params(params)
       return {} unless params
 
@@ -18,6 +19,8 @@ module PostQuery
       }
     end
 
+    # Formata a ordem de classificação dos resultados.
+    # Se nenhum parâmetro de ordem é especificado, a ordenação padrão é por ID em ordem decrescente (da maior para a menor).
     def format_order(order = '')
       return Arel.sql(order) if order.present?
 
